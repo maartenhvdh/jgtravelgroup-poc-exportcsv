48d0eb690ee23d2aab3df16707f7bcf0193f969e
Maarten van den Hooven <maartenv@kontent.ai>
2024-03-18 09:09:40 +0100

    change url

--- components/custom-elements/export-tours.tsx
+++ components/custom-elements/export-tours.tsx
@@ -12,7 +12,7 @@ export const ExportToursCustomElement: React.FC<IProps> = ({ element, context, v
     return <>
         <div className="custom-element">
             <div className="flex justify-center mb-3 p-1 py-5">
-                <a className="text-sm bg-gray-600 text-white py-1 px-3 rounded" href={`https://jgtravelgroup-poc-exportcsv-fbwt.vercel.app/api/export-tours-csv?codename=${context.item.codename}`}>Export all tours to CSV</a>
+                <a className="text-sm bg-gray-600 text-white py-1 px-3 rounded" href={`/api/export-tours-csv?codename=${context.item.codename}`}>Export all tours to CSV</a>
             </div>
         </div>
     </>
